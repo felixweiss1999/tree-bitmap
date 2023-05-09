0eaa2f249105cadac3ae634a1d03b058c46ba935
felixwhite <klassemax@gmail.com>
2023-05-09 15:12:43 +0800

    corrected tree node

--- main.c
+++ main.c
@@ -8,21 +8,11 @@ https://raminaji.wordpress.com/tree-bitmap/
 #define WORD_SIZE 32 // assuming 32-bit machine
 
 typedef struct TreeNode {
-    uint32_t bitmap; // bitmap of the node
-    int height; // height of the node in the tree
-    struct TreeNode* left; // left child node
-    struct TreeNode* right; // right child node
+    uint16_t child_exists; // child node existence bitmap
+    uint16_t prefix_exists; // prefix bitmap
+    uint32_t* hoparr; // pointer to nexthop array
+    TreeNode* childblock; // pointer to child block
 } TreeNode;
-
-// create a new tree node with the given bitmap and height
-TreeNode* createNode(uint32_t bitmap, int height) {
-    TreeNode* node = (TreeNode*) malloc(sizeof(TreeNode));
-    node->bitmap = bitmap;
-    node->height = height;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-}
 
 // set the ith bit of the bitmap of the given node
 void setBit(TreeNode* node, int i) {
@@ -71,45 +61,3 @@ int select(TreeNode* node, int r) {
     return -1;
 }
 
-// insert a new element with the given value into the tree rooted at the given node
-void insert(TreeNode** nodePtr, int value) {
-    TreeNode* node = *nodePtr;
-    if (node == NULL) {
-        *nodePtr = createNode(0, 0);
-        setBit(*nodePtr, value);
-        return;
-    }
-    int heightDiff = 0;
-    if (value < WORD_SIZE / 2) {
-        insert(&node->left, value);
-        heightDiff = node->left->height - node->height;
-        if (heightDiff == 1) {
-            if (node->left->bitmap == (1 << WORD_SIZE / 2) - 1) {
-                node->left = createNode(0, node->height + 1);
-                node->left->left = createNode(0, node->height);
-                node->left->right = createNode(0, node->height);
-                node->left->left->bitmap = (1 << WORD_SIZE / 2) - 1;
-                node->left->right->bitmap = node->bitmap & ((1 << WORD_SIZE / 2) - 1);
-                node->bitmap = node->bitmap >> WORD_SIZE / 2;
-                node->left->right->bitmap = node->left->right->bitmap >> WORD_SIZE / 2;
-            }
-        }
-    } else {
-        insert(&node->right, value - WORD_SIZE / 2);
-        heightDiff = node->right->height - node->height;
-        if (heightDiff == 1) {
-            if (node->right->bitmap == (1 << WORD_SIZE / 2) - 1) {
-                node->right = createNode(0, node->height + 1);
-                node->right->left = createNode(0, node->height);
-                node->right->right = createNode(0, node->height);
-                node->right->left->bitmap = node->bitmap >> WORD_SIZE / 2;
-                node->right->right->bitmap = (1 << WORD_SIZE / 2) - 1;
-                node->bitmap = node->bitmap & ((1 << WORD_SIZE / 2) - 1);
-                node->right->left->bitmap = node->right->left->bitmap & ((1 << WORD_SIZE / 2) - 1);
-            }
-        }
-    }
-    if (heightDiff == 0) {
-        setBit(node, value % WORD_SIZE);
-    }
-}
