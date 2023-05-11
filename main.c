64734be97fa35b5e589616fbaf531a153938a802
felixwhite <klassemax@gmail.com>
2023-05-11 13:25:05 +0800

    clean up, added table.c for handling table setup

--- main.c
+++ main.c
@@ -1,63 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-/*
-links to consider:
-https://raminaji.wordpress.com/tree-bitmap/
-*/
-#define WORD_SIZE 32 // assuming 32-bit machine
 
 typedef struct TreeNode {
-    uint16_t child_exists; // child node existence bitmap
-    uint16_t prefix_exists; // prefix bitmap
-    uint32_t* hoparr; // pointer to nexthop array
-    TreeNode* childblock; // pointer to child block
+    uint16_t child_exists;
+    uint16_t prefix_exists;
+    uint32_t* hoparr;
+    TreeNode* childblock;
 } TreeNode;
 
-// set the ith bit of the bitmap of the given node
-void setBit(TreeNode* node, int i) {
-    node->bitmap |= (1 << i);
-}
-
-// clear the ith bit of the bitmap of the given node
-void clearBit(TreeNode* node, int i) {
-    node->bitmap &= ~(1 << i);
-}
-
-// check if the ith bit of the bitmap of the given node is set
-int getBit(TreeNode* node, int i) {
-    return (node->bitmap >> i) & 1;
-}
-
-// count the number of set bits in the bitmap of the given node
-int countBits(TreeNode* node) {
-    int count = 0;
-    for (int i = 0; i < WORD_SIZE; i++) {
-        count += getBit(node, i);
-    }
-    return count;
-}
-
-// get the rank of the ith bit in the bitmap of the given node
-int rank(TreeNode* node, int i) {
-    int rank = 0;
-    for (int j = 0; j <= i; j++) {
-        rank += getBit(node, j);
-    }
-    return rank;
-}
-
-// find the position of the rth set bit in the bitmap of the given node
-int select(TreeNode* node, int r) {
-    int count = 0;
-    for (int i = 0; i < WORD_SIZE; i++) {
-        if (getBit(node, i)) {
-            count++;
-            if (count == r) {
-                return i;
-            }
-        }
-    }
-    return -1;
-}
 
---
+++ table.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+struct TABLEENTRY{
+	uint32_t ip;
+	char len;
+	char port;
+};
+
+void read_table(char *str,unsigned int *ip,int *len,unsigned int *nexthop){
+	char tok[]="./";
+	char buf[100],*str1;
+	unsigned int n[4];
+	sprintf(buf,"%s\0",strtok(str,tok));
+	n[0]=atoi(buf);
+	sprintf(buf,"%s\0",strtok(NULL,tok));
+	n[1]=atoi(buf);
+	sprintf(buf,"%s\0",strtok(NULL,tok));
+	n[2]=atoi(buf);
+	sprintf(buf,"%s\0",strtok(NULL,tok));
+	n[3]=atoi(buf);
+	*nexthop=n[2];
+	str1=(char *)strtok(NULL,tok);
+	if(str1!=NULL){
+		sprintf(buf,"%s\0",str1);
+		*len=atoi(buf);
+	}
+	else{
+		if(n[1]==0&&n[2]==0&&n[3]==0)
+			*len=8;
+		else
+			if(n[2]==0&&n[3]==0)
+				*len=16;
+			else
+				if(n[3]==0)
+					*len=24;
+	}
+	*ip=n[0];
+	*ip<<=8;
+	*ip+=n[1];
+	*ip<<=8;
+	*ip+=n[2];
+	*ip<<=8;
+	*ip+=n[3];
+}
+
+struct TABLEENTRY* set_table(char *file_name){
+	FILE *fp;
+    int num_entry = 0, less16=0, equal16=0;
+	int len;
+	char string[100];
+	uint32_t ip,nexthop;
+	fp=fopen(file_name,"r");
+	while(fgets(string,50,fp)!=NULL){
+		read_table(string,&ip,&len,&nexthop);
+		num_entry++;
+	}
+	rewind(fp);
+	struct TABLEENTRY* table = (struct TABLEENTRY *)malloc(num_entry*sizeof(struct TABLEENTRY));
+	num_entry=0;
+	while(fgets(string,50,fp)!=NULL){
+		read_table(string,&ip,&len,&nexthop);
+		if(len<16)
+			less16++;
+		else if(len==16)
+			equal16++;
+		table[num_entry].ip=ip;
+		table[num_entry].port=nexthop;
+		table[num_entry++].len=len;
+	}
+    return table;
+}
